6d91ffe32350af223507dd2f9e10a2a046c9ca66
Kuba <jakub.niedzwiedzki1990@gmail.com>
2019-05-16 23:47:01 +0200

    Updating json files with current results part I

--- server.js
+++ server.js
@@ -3,40 +3,42 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
-const groupManagementRouter = require('./routes/api/groupManagement')
-const registerLoginRouter = require('./routes/api/register-login')
-const invitedRouter = require('./routes/api/invited')
-const deleteRegainRouter = require('./routes/api/delete-regain')
-const resultsRouter = require('./routes/api/results')
+const groupManagementRouter = require('./routes/api/groupManagement');
+const registerLoginRouter = require('./routes/api/register-login');
+const invitedRouter = require('./routes/api/invited');
+const deleteRegainRouter = require('./routes/api/delete-regain');
+const resultsRouter = require('./routes/api/results');
+const updateJSONfiles = require('./updateJSONfiles/updateJSONfiles');
+updateJSONfiles();
 
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB Config
-const db = require('./config/keys').MONGODB_URL
+const db = require('./config/keys').MONGODB_URL;
 
 //Connect to MongoDB
 mongoose
-    .connect(db, {useNewUrlParser: true})
-    .then(() =>console.log('mongoDB connected'))
-    .catch(err => console.log(err))
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('mongoDB connected'))
+  .catch(err => console.log(err));
 
 //Passport middleware
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 // Passport config
-require('./config/passport.js')(passport)
+require('./config/passport.js')(passport);
 
 //Use Routes
-app.use(groupManagementRouter)
-app.use(registerLoginRouter)
-app.use(invitedRouter)
-app.use(deleteRegainRouter)
-app.use(resultsRouter)
+app.use(groupManagementRouter);
+app.use(registerLoginRouter);
+app.use(invitedRouter);
+app.use(deleteRegainRouter);
+app.use(resultsRouter);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
